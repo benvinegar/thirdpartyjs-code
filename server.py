acb388fb5533f88f4898eb6dfcb31a102d93b540
Ben Vinegar <ben@benv.ca>
2012-01-19 01:21:52 -0800

    Fix Chapter 6 examples (sessions)

--- server.py
+++ server.py
@@ -5,20 +5,33 @@ from flask import Flask, render_template, make_response, request
 
 app = Flask(__name__, static_url_path='/examples', static_folder='build')
 
+
 @app.route('/slow')
 def slow():
     """
-    	Wait 3 seconds before redirecting to the requested resource
+    Wait 3 seconds before redirecting to the requested resource
     """
     time.sleep(3)
     return flask.redirect(request.args.get('url'))
 
+
 @app.route('/echo.jsonp')
 def jsonp():
-	"""
-		Returns the passed query string args via JSONP.
-	"""
-	return "%s(%s)" % (request.args.get('callback'), json.dumps(request.args))
+    """
+    Returns the passed query string args via JSONP.
+    """
+    return "%s(%s)" % (request.args.get('callback'), json.dumps(request.args))
+
+
+@app.route('/login',  methods=['POST'])
+def login():
+    """
+    Sets a cookie
+    """
+    # return flask.redirect('/examples/06/auth-new-window/success.html')
+    r = make_response(flask.redirect('/examples/06/auth-new-window/success.html'))
+    r.set_cookie('session_id', 'abcdef123456')
+    return r
 
 if __name__ == '__main__':
     app.run()
