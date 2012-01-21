8d3b84379ee69820fe696a1bf20d93f9071c2640
Ben Vinegar <ben@benv.ca>
2012-01-21 14:29:20 -0800

    Use Twitter bootstrap

--- examples/templates/07/simple-xss/simple_xss.js
+++
@@ -1,5 +0,0 @@
-function load() {
-    var review = document.getElementById('review');
-    review.innerHTML = "This camera is great!";
-    review.innerHTML += "<script>alert('Hello, World!');</script>";
-}
